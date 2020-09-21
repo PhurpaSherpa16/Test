fb76476692f5f56e5799080cb6163037e89d8e42
EUDY MONGZ <phurpasherpa900@gmail.com>
2020-09-22 02:02:33 +0545

    First Commit

---
+++ index.py
@@ -0,0 +1,11 @@
+#modules importing
+import calculations as c
+num = int(input("Enter 1st number : "))
+num2 = int(input("Enter 2nd number : "))
+
+sum = c.sum(num,num2)
+print("Sum :",sum)
+sub =c.sub(num,num2)
+print("Subtraction :",sub)
+multiply = c.multiply(num,num2)
+print("Multipication :",multiply)
